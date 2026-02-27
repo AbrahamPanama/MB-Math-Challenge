c81dae2a812950aa69550978b171455b5cecfc04
Abraham Saenz <abraham.saenz@gmail.com>
2026-02-27 04:06:11 +0000

    Try fixing this error: `Runtime Error: useLanguage must be used within a

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,7 @@
-import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
-import { SidebarNav } from '@/components/layout/sidebar-nav';
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider>
-      <SidebarNav />
-      <SidebarInset>
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <>{children}</>;
 }
